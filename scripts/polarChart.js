ef9da780ba3aacb8db1efc81e87d8b5770f1187f
Jeffrey Leung <jleung513@gmail.com>
2017-02-09 23:33:25 -0800

    [Polar] Add optional labels to arcs

--- scripts/polarChart.js
+++ scripts/polarChart.js
@@ -17,10 +17,15 @@ var svg = d3.select("#polarChart").append("svg")
  * @param {Array} array Array of values to be charted
  * @param {Number} x X-coordinate of the center point of the arc
  * @param {Number} y Y-coordinate of the center point of the arc
- * @param {String} color (Optional) Name of the color to draw the chart in; black if not specified
+ * @param {String} color (Optional) Name of the color to draw the chart in;
+ * black if not specified
+ * @param {Array} textArray (Optional) Array of Strings to be placed on the
+ * outside of the arcs
+ * @param {String} textColor (Optional) Name of the color to draw the text in;
+ * black if not specified
  *
  */
-function drawPolarChart(array, x, y, color) {
+function drawPolarChart(array, x, y, color, textArray, textColor) {
 
   /**
    * Draws an arc for the D3 object svg
@@ -32,30 +37,65 @@ function drawPolarChart(array, x, y, color) {
    * @param {Number} endAngle Ending angle of the arc
    * @param {Number} x X-coordinate of the center point of the arc
    * @param {Number} y Y-coordinate of the center point of the arc
-   * @param {String} color (Optional) Name of the color to draw the arc in,
+   * @param {String} arcColor (Optional) Name of the color to draw the arc in;
+   * black if not specified
+   * @param {String} text (Optional) Text to place on the outside of the arc
+   * @param {String} textColor (Optional) Name of the color to draw the text in;
    * black if not specified
    *
    */
-  function drawArc(arcSize, startAngle, endAngle, x, y, color) {
+  function drawArc(arcSize, startAngle, endAngle, x, y, arcColor,
+      text, textColor) {
+
     if(color === null) {
       color = "black";
     }
+    if(textColor === null) {
+      textColor = "black";
+    }
 
+    var elementId = "arc_" + Math.random().toString();
+
+    // Draw the arc
     var arc = d3.svg.arc()
         .innerRadius(0).outerRadius(arcSize)
         .startAngle(startAngle).endAngle(endAngle);
-
     var translateDirections = "translate(" + x.toString() + ", " + y.toString() + ")";
+
     svg.append("path")
+        .attr("id", elementId)
         .attr("d", arc)
         .style("fill", color)
         .style("stroke", "white")
         .attr("transform", translateDirections);
+
+    // Draw the text
+    if(text !== undefined || text !== null) {
+      svg.append("text").append("textPath")
+          .attr("xlink:href", "#"+elementId)
+          .attr("startOffset", "18%")
+          // .attr("dx", 20)
+          .style("text-anchor", "middle")
+          .style("stroke", textColor)
+          .text(text);
+    }
+  }
+
+  if(textArray === undefined) {
+    var textArray = [];
+  }
+  for(var i = textArray.length; i < array.length; i++) {
+    textArray.push(null);
   }
 
   var angle = (Math.PI*2)/array.length;
+
   for(var i = 0; i < array.length; i++) {
-    drawArc(array[i], i*angle, i*angle + angle, x, y, color);
+    drawArc(
+      array[i], i*angle, i*angle + angle,
+      x, y, color,
+      textArray[i], textColor
+    );
   }
 }
 
@@ -79,6 +119,34 @@ function arrayOfRandomIntegers(length, lower, upper) {
   return array;
 }
 
-drawPolarChart(arrayOfRandomIntegers(5, 175, 225), xCenter, yCenter);
-drawPolarChart(arrayOfRandomIntegers(5, 100, 150), xCenter, yCenter, "green");
-drawPolarChart(arrayOfRandomIntegers(5, 25, 75), xCenter, yCenter, "blue");
+/**
+ * Generates array with stringified values
+ *
+ * @param {Array} array Array of values
+ * @return {Array} array Array of String values
+ *
+ */
+function arrayToStringArray(array) {
+  var arrayStrings = [];
+  for(var i = 0; i < array.length; i++) {
+    arrayStrings.push(array[i].toString())
+  }
+  return arrayStrings;
+}
+
+var values = [];
+
+values = arrayOfRandomIntegers(5, 175, 225);
+drawPolarChart(values, xCenter, yCenter, "black",
+    arrayToStringArray(values), "black"
+);
+
+values = arrayOfRandomIntegers(5, 100, 150);
+drawPolarChart(values, xCenter, yCenter, "green",
+    arrayToStringArray(values), "white"
+);
+
+values = arrayOfRandomIntegers(5, 25, 75);
+drawPolarChart(values, xCenter, yCenter, "blue",
+    arrayToStringArray(values), "steelblue"
+);
